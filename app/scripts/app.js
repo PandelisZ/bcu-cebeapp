08378faaf29f1a1fd85bbae4237ed6d109af6db9
Pandelis Zembashis <info@pandelis.me>
2016-08-18 09:56:41 +0100

    intial build

---
+++ app/scripts/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cebeApp
+ * @description
+ * # cebeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cebeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/cebeRoute', {
+        templateUrl: 'views/ceberoute.html',
+        controller: 'CeberouteCtrl',
+        controllerAs: 'cebeRoute'
+      })
+      //Email page for requestion assitance from SSA central team
+      .when('/email', {
+        templateUrl: 'views/email.html',
+        controller: 'EmailCtrl',
+        controllerAs: 'email'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cebeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cebeApp
+ */
+angular.module('cebeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
